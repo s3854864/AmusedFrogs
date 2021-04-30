3f6e20dcfb64d18765777bb7e4d248c9d77ee8e6
HOANG TRAN s3720825 <s3720825@student.rmit.edu.au>
2021-04-30 18:31:04 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="frogs.css">
+<link rel="stylesheet" href="bootstrap.css">
 <title>PowerCore Digital </title>
 </head>
 
 <body>
-<header> PowerCore Digital</header>
+<header class="section-header">
 
+<section class="header-main border-bottom">
+	<div class="container">
+<div class="row align-items-center">
+	<div class="col-lg-2 col-6">
+		<a href="http://bootstrap-ecommerce.com" class="brand-wrap">
+			<!-- <img class="logo" padding="20px" src="images/logoblue.png"> -->
+		</a> <!-- brand-wrap.// -->
+	</div>
+	<div class="col-lg-6 col-12 col-sm-12">
+		<form action="#" class="search">
+			<div class="input-group w-100">
+			    <input type="text" class="form-control" placeholder="Search">
+			    <div class="input-group-append">
+			      <button class="btn btn-primary" type="submit">
+			        <i class="fa fa-search"></i> Search
+			      </button>
+			    </div>
+		    </div>
+		</form> <!-- search-wrap .end// -->
+	</div> <!-- col.// -->
+	<div class="col-lg-4 col-sm-6 col-12">
+		<div class="widgets-wrap float-lg-right">
+			<div class="widget-header  mr-3">
+				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+				<span class="badge badge-pill badge-danger notify">0</span>
+			</div>
+			<div class="widget-header icontext">
+				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
+				<div class="text">
+					<span class="text-muted">Welcome!</span>
+					<div> 
+						<a href="#">Sign in</a> |  
+						<a href="#"> Register</a>
+					</div>
+				</div>
+			</div>
+		</div> <!-- widgets-wrap.// -->
+	</div> <!-- col.// -->
+</div> <!-- row.// -->
+	</div> <!-- container.// -->
+</section> <!-- header-main .// -->
+</header> <!-- section-header.// -->
 
 
 
