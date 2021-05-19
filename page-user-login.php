9c502fca40f47accba757f571d5488c8cbe0471e
3845279 <83212406+3845279@users.noreply.github.com>
2021-05-19 23:32:57 +1000

    Update page-user-login.php

--- page-user-login.php
+++ page-user-login.php
@@ -1,123 +1,132 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="pragma" content="no-cache" />
-<meta http-equiv="cache-control" content="max-age=604800" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
 
-<title>PowerCore Digital Sing In</title>
+	session_start();
 
-<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+	$username = "inte";
+	$password = "2047";
+	$errorMessage = "Invalid Credentials! Please Try Again";
 
-<!-- jQuery -->
-<script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
-
-<!-- Bootstrap4 files-->
-<script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-
-<!-- Font awesome 5 -->
-<link href="fonts/fontawesome/css/all.min.css" type="text/css" rel="stylesheet">
-
-<!-- custom style -->
-<link href="css/ui.css" rel="stylesheet" type="text/css"/>
-<link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
-
-<!-- custom javascript -->
-<script src="js/script.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-/// some script
-
-// jquery ready start
-$(document).ready(function() {
-	// jQuery code
-
-}); 
-// jquery end
-</script>
-<script>
-	function passwordFunction() {
-		var x = document.getElementById("user_password");
-  		if (x.type === "password") {
-    	x.type = "text"; //this is basically stating that when the variable x, which is the input field, is of the type "password", on click of button, the input type of x will change to type = text and the characters of this input field will be made available to see. 
-  		} else {
-    	x.type = "password"; //this is stating that else, after something if the button is not clicked, the variable x remains of type = password and the characters entered into this field will be not-visible. 
+	if (isset($_SESSION['Logged_in']) && $_SESSION['Logged_in'] == true) {
+		header("Location: index.php");
 	}
+
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		if ($_POST['username'] == $username && $_POST['password'] == $password) {
+			$_SESSION['Logged_in'] = true;
+			header("Location: index.php");
+		} 
 }
-</script>
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PowerCore Digital</title>
+
+	<link href="/AmusedFrogs/assets/brand/favicon.ico" rel="shortcut icon" type="image/x-icon">
+	
+	<!-- jQuery -->
+	<script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
+	
+	<!-- Bootstrap4 files-->
+	<script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+	
+	<!-- Font awesome 5 -->
+	<link href="fonts/fontawesome/css/all.min.css" type="text/css" rel="stylesheet">
+	
+	<!-- custom style -->
+	<link href="css/ui.css" rel="stylesheet" type="text/css"/>
+	<link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+	
+	<!-- custom javascript -->
+	<script src="js/script.js" type="text/javascript"></script>
+	
+	<script type="text/javascript">
+	/// some script
+	
+	// jquery ready start
+	$(document).ready(function() {
+		// jQuery code
+	
+	}); 
+	// jquery end
+	</script>
 </head>
+
 <body>
+<header class="section-header">
 
-
-	<header class="section-header">
-
-		<section class="header-main border-bottom">
-			<div class="container">
-		<div class="row align-items-center">
-			<div class="col-lg-2 col-6">
-				<a href="index.php" style="font-size: 20px;">
-					<img class="logo"  src="assets/brand/logo/logoblue.png" style= "width: 80px;"> <br> PowerCore Digital
-				</a>
+<section class="header-main border-bottom">
+	<div class="container">
+<div class="row align-items-center">
+	<div class="col-lg-2 col-6">
+		<a href="index.php" style="font-size: 20px;">
+			<img class="logo"  src="assets/brand/logo/logoblue.png" style= "width: 80px;"> <br> PowerCore Digital
+		</a>
+	</div>
+	<div class="col-lg-6 col-12 col-sm-12">
+		<form action="#" class="search">
+			<div class="input-group w-100">
+			    <input type="text" class="form-control" placeholder="Search">
+			    <div class="input-group-append">
+			      <button class="btn btn-primary" type="submit">
+			        <i class="fa fa-search"></i> Search
+			      </button>
+			    </div>
+		    </div>
+		</form> <!-- search-wrap .end// -->
+	</div> <!-- col.// -->
+	<div class="col-lg-4 col-sm-6 col-12">
+		<div class="widgets-wrap float-lg-right">
+			<div class="widget-header2  mr-3">
+				<a href="page-user-login.php" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+				<span class="badge badge-pill badge-danger notify">0</span>
 			</div>
-			<div class="col-lg-6 col-12 col-sm-12">
-				<form action="#" class="search">
-					<div class="input-group w-100">
-						<input type="text" class="form-control" placeholder="Search">
-						<div class="input-group-append">
-						  <button class="btn btn-primary" type="submit">
-							<i class="fa fa-search"></i> Search
-						  </button>
-						</div>
+			<div class="widget-header3 icontext">
+				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
+			</div>
+			<div class="text-index4">
+				<span class="text-muted">Welcome!</span>
+					<div class="php"> 
+						<pre class="top-right-style"><a class="profile-index" href="#">Login</a> | <a class="profile-index" href="page-user-register.php">Register</a></pre>
 					</div>
-				</form> <!-- search-wrap .end// -->
-			</div> <!-- col.// -->
-			<div class="col-lg-4 col-sm-6 col-12">
-				<div class="widgets-wrap float-lg-right">
-					<div class="widget-header  mr-3">
-						<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
-						<span class="badge badge-pill badge-danger notify">0</span>
-					</div>
-					<div class="widget-header icontext">
-						<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
-						<div class="text">
-							<span class="text-muted">Welcome!</span>
-							<div> 
-								<a href="#">Sign in</a> |  
-								<a href="#"> Register</a>
-							</div>
-						</div>
-					</div>
-				</div> <!-- widgets-wrap.// -->
-			</div> <!-- col.// -->
-		</div> <!-- row.// -->
-			</div> <!-- container.// -->
-		</section> <!-- header-main .// -->
-		</header> <!-- section-header.// -->
- 
-		  </header> <!-- section-header.// -->
-
+			</div>
+		</div> <!-- widgets-wrap.// -->
+	</div> <!-- col.// -->
+</div> <!-- row.// -->
+	</div> <!-- container.// -->
+</section> <!-- header-main .// -->
+</header> <!-- section-header.// -->
 
 
 <!-- ========================= SECTION CONTENT ========================= -->
 <section class="section-conten padding-y" style="min-height:84vh">
 
 <!-- ============================ COMPONENT LOGIN   ================================= -->
-	<div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
+	<div class="card mx-auto" style="max-width: 380px; margin-top:65px;">
       <div class="card-body">
       <h2 class="card-title mb-4">Sign in</h2>
-      <form method="POST" action="page-user-login.php">
-      	  <a href="#" class="btn btn-facebook btn-block mb-2"> <i class="fab fa-facebook-f"></i> &nbsp  Sign in with Facebook</a>
+      <form method="POST" action="page-user-login.php">   	  
+		
+		  <a href="#" class="btn btn-facebook btn-block mb-2"> <i class="fab fa-facebook-f"></i> &nbsp  Sign in with Facebook</a>
       	  <a href="#" class="btn btn-google btn-block mb-4"> <i class="fab fa-google"></i> &nbsp  Sign in with Google</a>
           <div class="form-group">
-			 <input name="username" class="form-control" placeholder="Username" type="text" id="user_username">
+			 <input name="username" class="form-control" placeholder="Username" type="text" id="user_username" required>
           </div> <!-- form-group// -->
           <div class="form-group">
-			<input name="password" class="form-control" placeholder="Password" type="password" id="user_password">
+			<input name="password" class="form-control" placeholder="Password" type="password" id="user_password" required>
 			<button type="button" class="show-password" onclick="passwordFunction()">
 				<i class="far fa-eye-slash"></i>
 			</button>
+
+			<br><div style="color:white;text-align:center;background-color:#ffcccb;border-radius:0.37rem;"><?php if (isset($_POST['username']) && isset($_POST['password'])) {
+			if ($_POST['username'] !== $username && $_POST['password'] !== $password) {
+				echo "<p style='padding-top:8px;padding-bottom:8px;font-weight:bold;font-family: Inter, Arial, Helvetica Neue, Segoe UI, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;'> Invalid Credentials!  Please Try Again </p>";
+			}	
+			}
+			?></div>
+
           </div> <!-- form-group// -->
           
           <div class="form-group">
@@ -125,13 +134,13 @@ $(document).ready(function() {
             <label class="float-left custom-control custom-checkbox"> <input type="checkbox" class="custom-control-input" checked=""> <div class="custom-control-label"> Remember </div> </label>
           </div> <!-- form-group form-check .// -->
           <div class="form-group">
-              <input type="submit" name="submit" class="btn btn-primary btn-block" value=Login>
+              <input type="submit" name="submit" class="btn btn-primary btn-block" value=Login>	
           </div> <!-- form-group// -->    
       </form>
       </div> <!-- card-body.// -->
     </div> <!-- card .// -->
 
-     <p class="text-center mt-4">Don't have account? <a href="#">Register</a></p>
+     <p class="text-center mt-4">Don't have account? <a href="page-user-register.php">Register</a></p>
      <br><br>
 <!-- ============================ COMPONENT LOGIN  END.// ================================= -->
 
@@ -159,16 +168,16 @@ $(document).ready(function() {
 				<aside class="col-md col-6">
 					<h6 class="title">Company</h6>
 					<ul class="list-unstyled">
-						<li> <a href="page-about-us.html">About us</a></li>
+						<li> <a href="page-about-us.php">About us</a></li>
 						<li> <a href="#">Career</a></li>
 						<li> <a href="#">Rules and terms</a></li>
-						<li> <a href="sitemap.html">Sitemap</a></li>
+						<li> <a href="sitemap.php">Sitemap</a></li>
 					</ul>
 				</aside>
 				<aside class="col-md col-6">
 					<h6 class="title">Help</h6>
 					<ul class="list-unstyled">
-						<li> <a href="contactus.html">Contact us</a></li>
+						<li> <a href="contactus.php">Contact us</a></li>
 						<li> <a href="#">Refund Policy</a></li>
 						<li> <a href="#">Order status</a></li>
 						<li> <a href="#">Shipping Info</a></li>
